7bc3579b173297b76c348fdab355e43bd1db1d88
apparao <apparao123.g@gmail.com>
2020-02-28 16:45:32 +0530

    useState with preState

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 function HookCounterTwo() {
   const initialCount = 0;
   const [count, setCount] = useState(initialCount);
+  //instead of prevCount , we can give any name.Even we can give count also.
+  //for setCount ,we are calling arrow fuction wit param (prevCount).This param holds the previous count value.
   const incrementByFive = () => {
     for (let i = 0; i < 5; i++) {
-      setCount(count + 1);
+      setCount(prevCount => prevCount + 1);
     }
   };
 
@@ -13,8 +15,12 @@ function HookCounterTwo() {
     <div>
       Count:{count}
       <button onClick={() => setCount(initialCount)}>Reset</button>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>
+        Decrement
+      </button>
       <button onClick={incrementByFive}>incrementByFive</button>
     </div>
   );
