3399fb667ddc244e656a117bbaaf044431a9bbc0
apparao <apparao123.g@gmail.com>
2020-02-28 16:32:24 +0530

    useState with in proper increment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HookCounter from "./components/HookCounter";
+import HookCounterTwo from "./components/HookCounterTwo";
 
 function App() {
   return (
     <div className='App'>
-      <HookCounter />
+      <HookCounterTwo />
     </div>
   );
 }
--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 function HookCounter() {
+  //0 is setting default value.
   const [count, setCount] = useState(0);
   return (
     <div>
