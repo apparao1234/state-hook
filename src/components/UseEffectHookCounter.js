8f6fa485f315dd93cf767a25c7764cde68f137d6
apparao <apparao123.g@gmail.com>
2020-03-02 13:09:52 +0530

    useEffect conditional execution

--- src/components/UseEffectHookCounter.js
+++ src/components/UseEffectHookCounter.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 
 function UseEffectHookCounter() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState();
 
   //This will be called after all renderings of the component.
   //it will replace life cycle methods of calss like componentDidMount(),componentDidUpdate(),componentWillUnmount()
   useEffect(() => {
+    console.log("in side useEffect");
     document.title = `you clicked ${count}times`;
-  });
+  }, [count]);
 
   return (
     <div>
+      <input type='text' value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(count + 1)}>click {count} times</button>
     </div>
   );
