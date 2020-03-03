50dfc8fa911fc0efbeaa075013033184faa7dbbc
apparao <apparao123.g@gmail.com>
2020-03-03 20:25:33 +0530

    Fetching data with useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
 
 function App() {
   return (
     <div className='App'>
-      <IntervalHookCounter />
+      <DataFetching />
     </div>
   );
 }
