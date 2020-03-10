2fc125b3176540f2137d6d78cbe12c75cfbc42ec
apparao <apparao123.g@gmail.com>
2020-03-10 16:50:03 +0530

    ReactHooks useReducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ComponentC from "./components/ComponentC";
-
-//the context is exported to use in other component
-//where consumer configured
-export const FirstnameContext = React.createContext();
-export const SurNameContext = React.createContext();
+import CounterReducer from "./components/CounterReducer";
 
 function App() {
   return (
     <div className='App'>
-      <FirstnameContext.Provider value={"Karunya"}>
-        <SurNameContext.Provider value={"G"}>
-          <ComponentC />
-        </SurNameContext.Provider>
-      </FirstnameContext.Provider>
+      <CounterReducer value={"Karunya"}></CounterReducer>
     </div>
   );
 }
